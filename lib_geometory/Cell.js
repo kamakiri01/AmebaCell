ce16a9657b166f0b9e260e86b54452bf02a5ded8
kamakiri01 <kamakiri0148@gmail.com>
2014-06-08 00:21:23 +0900

    fix and replace to module pattern.

--- lib_geometory/Cell.js
+++ lib_geometory/Cell.js
@@ -3,11 +3,12 @@
  * this lib need Geo.enchant.js
  *
  */
-
-var numberOfNodes = 200;
+var Geo = Geo;
 var lineLength = 80;
 var capLineLength = lineLength/2;
 var Cell = (function(){
+        var linelayer = {};
+        var numberOfNodes = 200;
         var candi = [];
         var cL = 0;
         var frag = true;
@@ -20,7 +21,129 @@ var Cell = (function(){
             capture:[], //捕獲されたセルのリスト
             capComed:false  //捕獲中の状態変数
         };
-        var CellsEntity = enchant.Class.create(Geo.Circle, {
+        var ForceMethods = (function(){
+                var calcForce = function(){
+                    //相互に受ける力が同じとは限らないので全配列ループする
+                    for(var i=0; i<Cell.numberOfNodes;i++){
+                        //受ける加速度の積
+                        var fx=0, fy=0;
+                        var nd0 = Cell.CellClass.collection[i];
+                        for(var j=0; j<Cell.numberOfNodes;j++){
+                            var nd1 = Cell.CellClass.collection[j];
+                            if(i !== j && nd0 !== undefined && nd1 !== undefined){
+                                //距離の計算
+                                var dx = nd1.x - nd0.x;
+                                var dy = nd1.y - nd0.y;
+                                var dist = Math.sqrt(dx * dx + dy * dy);
+                                //同種に近付く or 離れる
+                                //非常に近距離では反発する
+                                if(dist<40 && dist>1){
+                                    fx -= dx / dist / dist / dist * 40;
+                                    fy -= dy / dist / dist / dist * 40;
+                                    //200以上離れていたら同じタイプで引き合う
+                                }else if(dist<200 && nd0.type === nd1.type){
+                                    fx += dx / dist / dist / dist * 30;
+                                    fy += dy / dist / dist / dist * 30;
+                                }
+                                //なんとなく集合しようとする
+                                if(dist>1){
+                                    var vx = nd1.vx;
+                                    var vy = nd1.vy;
+                                    var rad = Math.atan2(vy,vx);
+                                    fx += Math.cos(rad) /dist/dist;
+                                    fy += Math.sin(rad) /dist/dist;
+                                }
+                                //加速度を集積
+                                nd0.fx += fx;
+                                nd0.fy += fy;
+                            }
+                        }
+                        //加速度をノーマライズ
+                        var fdist = Math.sqrt(nd0.fx * nd0.fx + nd0.fy * nd0.fy);
+                        if(fdist > 1){
+                            nd0.fx = nd0.fx/fdist;
+                            nd0.fy = nd0.fy/fdist;
+                        }
+                        //若干ランダムに運動
+                        nd0.fx += (Math.random()-0.5)*0.2;
+                        nd0.fy += (Math.random()-0.5)*0.2;
+                    }
+                };
+                var applyForce = function(){
+                    for(var i=0; i<Cell.numberOfNodes;i++){
+                        Cell.CellClass.collection[i].vx += Cell.CellClass.collection[i].fx;
+                        Cell.CellClass.collection[i].vy += Cell.CellClass.collection[i].fy;
+                        var vvx =  Cell.CellClass.collection[i].vx;
+                        var vvy =  Cell.CellClass.collection[i].vy;
+                        var vdist = Math.sqrt(vvx * vvx + vvy * vvy);
+                        //速度をノーマライズ
+                        if(vdist > 1){
+                            Cell.CellClass.collection[i].vx = vvx/vdist;
+                            Cell.CellClass.collection[i].vy = vvy/vdist;
+                        }
+
+                        //画面外に出る速度がある場合速度を反転
+                        var sumX = Cell.CellClass.collection[i].x + Cell.CellClass.collection[i].vx;
+                        var sumY = Cell.CellClass.collection[i].y + Cell.CellClass.collection[i].vy;
+                        if(sumX < 5 || sumX > enchant.Core.instance.width-5){
+                            Cell.CellClass.collection[i].vx *= -1;
+                        }
+                        if(sumY < 5 || sumY > enchant.Core.instance.height-5){
+                            Cell.CellClass.collection[i].vy *= -1;
+                        }
+
+                        //既に画面外にいる場合中心力をかける
+                        sumX -= Cell.CellClass.collection[i].vx;
+                        sumY -= Cell.CellClass.collection[i].vy;
+                        if(sumX < 0 || sumX > enchant.Core.instance.width){
+                            Cell.CellClass.collection[i].vx = (enchant.Core.instance.width - sumX)/10;
+                        }
+                        if(sumY < 0 || sumY > enchant.Core.instance.height){
+                            Cell.CellClass.collection[i].vy = (enchant.Core.instance.height - sumY)/10;
+                        }
+
+                        //クリック移動中は力積が適用されない（演算自体を飛ばしたほうが効率的）
+                        if(Cell.CellClass.collection[i].isCapture === true && Cell.userTouch.capComed === true){
+                            //nothing
+                        }else{
+                            //速度の適用
+                            Cell.CellClass.collection[i].x += Cell.CellClass.collection[i].vx;
+                            Cell.CellClass.collection[i].y += Cell.CellClass.collection[i].vy;
+                            //整数値にならす(GPU環境では特に不要)
+                            Cell.CellClass.collection[i].x = (Cell.CellClass.collection[i].x + 0.5) << 0;
+                            Cell.CellClass.collection[i].y = (Cell.CellClass.collection[i].y + 0.5) << 0;
+                        }
+                    }
+                }
+                var capturingCellsPosition = function(){
+                    if(Cell.userTouch.capComed === true){
+                        for(var i=0;i<Cell.userTouch.capture.length;i++){
+                            Cell.userTouch.capture[i].x = Cell.userTouch.x + Cell.userTouch.capture[i].rVecX;
+                            Cell.userTouch.capture[i].y = Cell.userTouch.y + Cell.userTouch.capture[i].rVecY;
+                        }
+                    }
+                }
+                return{
+                    calcForce: calcForce,
+                    applyForce: applyForce,
+                    capturingCellsPosition: capturingCellsPosition
+                }
+        })()
+        var addForceEvent = function(){
+            enchant.Core.instance.rootScene.addEventListener("enterframe", function(){
+                    if(Cell.duringCapture === false){
+                        //力積の生成
+                        //相互に受ける力が同じとは限らないので全配列ループする
+                        Cell.ForceMethods.calcForce();
+                        //力積の適用
+                        Cell.ForceMethods.applyForce();
+                        //力積の適用項内だとクリック時の相対座標保持に問題がありそうなので、別個に処理
+                        Cell.ForceMethods.capturingCellsPosition();
+                    }
+            });
+        }
+
+        var CellClass = enchant.Class.create(Geo.Circle, {
                 initialize: function(){
                     Geo.Circle.call(this, 2);
                     //セル表示座標
@@ -42,6 +165,22 @@ var Cell = (function(){
                     this.rVecY = 0;
                 }
         });
+        var addLineLayer = function(){
+            Cell.linelayer = new Geo.LineLayer(enchant.Core.instance.width, enchant.Core.instance.height);
+            enchant.Core.instance.rootScene.addChild(Cell.linelayer);
+        };
+        var addDrawLineEvent = function(){
+            enchant.Core.instance.rootScene.addEventListener("enterframe", function(){
+                    Cell.linelayer.areaWipe();
+                    //重複描画を避ける
+                    for(var i=0; i<Cell.numberOfNodes;i++){
+                        for(var j=Cell.numberOfNodes-1; j>=i;j--){
+                            //描画メソッド
+                            Geo.drawLine(Cell.CellClass.collection[i], Cell.CellClass.collection[j]);
+                        }
+                    }
+            });
+        }
         var startUserTouch = function(e){
             //ユーザーの初期化
             Cell.userTouch.x = e.x;
@@ -55,9 +194,9 @@ var Cell = (function(){
         };
         var createDeepCopy = function(){
             var result = [];
-            var l = Geo.Circle.collection.length;
+            var l = Cell.CellClass.collection.length;
             for(var i=0;i<l;i++){
-                result.push(Geo.Circle.collection[i]);
+                result.push(Cell.CellClass.collection[i]);
             }
             return result;
         };
@@ -126,6 +265,12 @@ var Cell = (function(){
         var addNewCell = function(cell){
             enchant.Core.instance.rootScene.addChild(cell);
         };
+        var addNewCells = function(){
+            for(var i=0; i<Cell.numberOfNodes;i++){
+                var cell = new Cell.CellClass();
+                Cell.addNewCell(cell);
+            }
+        }
         var getDistBetween2Cell = function(c1, c2){
             var dx = c1.x - c2.x;
             var dy = c1.y - c2.y;
@@ -133,18 +278,24 @@ var Cell = (function(){
             return dist;
         };
         var recetCellCapturingStatus = function(){
-            var l = Geo.Circle.collection.length;
+            var l = Cell.CellClass.collection.length;
             for(var i=0;i<l;i++){
-                Geo.Circle.collection[i].isCapture = false;
+                Cell.CellClass.collection[i].isCapture = false;
             }
         };
         return {
+            linelayer: linelayer,
+            numberOfNodes: numberOfNodes,
             candi: candi,
             cL: cL,
             frag: frag,
             candiBuf: candiBuf,
             duringCapture: duringCapture,
-            CellsEntity: CellsEntity,
+            ForceMethods: ForceMethods,
+            addForceEvent: addForceEvent,
+            CellClass: CellClass,
+            addLineLayer: addLineLayer,
+            addDrawLineEvent: addDrawLineEvent,
             userTouch: userTouch,
             startUserTouch: startUserTouch,
             recetUserTouch: recetUserTouch,
@@ -156,6 +307,7 @@ var Cell = (function(){
             captureCandidateCells: captureCandidateCells,
             captureLoopPhase: captureLoopPhase,
             addNewCell: addNewCell,
+            addNewCells: addNewCells,
             getDistBetween2Cell: getDistBetween2Cell,
             recetCellCapturingStatus: recetCellCapturingStatus
         
@@ -171,18 +323,16 @@ Cell.addTouchEvent = function(){
             //ユーザー状態の初期化
             Cell.recetUserTouch();
     });
-
     //捕獲中はドラッグドロップ
     enchant.Core.instance.rootScene.addEventListener('touchmove', function(e){
             Cell.userTouch.x = e.x;
             Cell.userTouch.y = e.y;
     });
-
     enchant.Core.instance.rootScene.addEventListener('touchstart', function(e){
             //ユーザー操作状態の初期化
             Cell.startUserTouch(e);
             //捕獲候補リストを先に作っておく
-            Cell.cL = Geo.Circle.collection.length;
+            Cell.cL = Cell.CellClass.collection.length;
             //ディープコピーを作る
             Cell.candi = Cell.createDeepCopy();
             //結合距離の半分以内にいるセルをリストアップ
@@ -191,39 +341,8 @@ Cell.addTouchEvent = function(){
             console.log("first: "+ Cell.userTouch.capture.length);
             //追加されたセルに結合しているセルを更にリストに追加する
             Cell.initDetectCellPhase();
-
+            //近隣のセルがなくなるまで再帰的に探索する
             Cell.captureLoopPhase();
-//            while(Cell.frag == true){
-//                //リストのそれぞれの要素から候補までの距離を調べ、近ければリストに追加し候補から外す（演算）
-//                //まずリストを一巡してリストを増やし、更にそのリストで候補を洗いなおす
-//                //新しく追加されるセルがなくなるまでこれを繰り返す
-//                
-//                //captureリスト全てに対して行う
-//                for(var i=0, ie=Cell.userTouch.capture.length;i<ie;i++){
-//                    //全てのcandiリストに対して行う
-//                    for(var j=0;j<Cell.candi.length;j++){
-//                        //同色のセルか確認
-//                        if(Cell.candi[j].type === Cell.userTouch.capture[i].type){
-//                            var dist = Cell.getDistBetween2Cell(Cell.candi[j], Cell.userTouch.capture[i]);
-//                            //candiリストの要素がcaptureリストの要素と近ければ捕獲する
-//                            if(dist !== 0 && dist < capLineLength){
-//                                //ノード内のセルと近ければ捕獲し、候補から外す
-//                                Cell.addCellToCandiBuf(Cell.candi[j], j);
-//                            }
-//                        }
-//                    }
-//                }
-//                //ループを続けるか判断
-//                if(Cell.candiBuf.length === 0){
-//                    console.log("no new capture");
-//                    //新しいセルが捕獲できなければ走査ループ終了
-//                    Cell.endDetectCellPhase();
-//                }else{
-//                    console.log("new capture "+Cell.candiBuf.length);
-//                    //新しく捕獲したセルをリストに追加する
-//                    Cell.captureCandidateCells();
-//                }
-//            }
             //クリック時のマウス座標とセルの相対座標を保存する
             Cell.userTouchLength = Cell.userTouch.capture.length;
             for(var i=0;i<Cell.userTouchLength;i++){
--- script.js
+++ script.js
@@ -1,134 +1,22 @@
+var Cell = Cell;
+var Geo = Geo;
+
 enchant();
 window.onload = function() {
     var game = new Game(1124, 700);
     game.fps = 30;
     game.rootScene.backgroundColor = "lightgray";
     game.onload = function(){
+        //タッチイベントの作成
         Cell.addTouchEvent();
-
         //Cellの生成
-        for(var i=0; i<numberOfNodes;i++){
-            var cell = new Cell.CellsEntity();
-            Cell.addNewCell(cell);
-        };
-        var linelayer = new Geo.LineLayer(enchant.Core.instance.width, enchant.Core.instance.height);
-        game.rootScene.addChild(linelayer);
+        Cell.addNewCells();
+        //描画レイヤーの作成
+        Cell.addLineLayer();
         //ライン描画イベント
-        game.rootScene.addEventListener('enterframe', function(){
-            var sCtx = linelayer.image.context;
-            sCtx.clearRect(0,0,enchant.Core.instance.width, enchant.Core.instance.height);
-            //重複描画を避ける
-            for(var i=0; i<numberOfNodes;i++){
-                for(var j=numberOfNodes-1; j>=i;j--){
-                    //描画メソッド
-                    Geo.drawLine(Geo.Circle.collection[i], Geo.Circle.collection[j]);
-                };
-            };
-        });
-
-        //セル間の相互作用による運動
-        game.rootScene.addEventListener('enterframe', function(){
-            if(Cell.duringCapture == false){
-                //力積の生成
-                //相互に受ける力が同じとは限らないので全配列ループする
-                for(var i=0; i<numberOfNodes;i++){
-                //受ける加速度の積
-                var fx=0, fy=0;
-                var nd0 = Geo.Circle.collection[i];
-                for(var j=0; j<numberOfNodes;j++){
-                var nd1 = Geo.Circle.collection[j];
-                    if(i != j && nd0 != undefined && nd1 != undefined){
-                    //距離の計算
-                    var dx = nd1.x - nd0.x;
-                    var dy = nd1.y - nd0.y;
-                    var dist = Math.sqrt(dx * dx + dy * dy);
-                    //同種に近付く or 離れる
-                    //非常に近距離では反発する
-                    if(dist<40 && dist>1){
-                        fx -= dx / dist / dist / dist * 40;
-                        fy -= dy / dist / dist / dist * 40;
-                        //200以上離れていたら同じタイプで引き合う
-                    }else if(dist<200 && nd0.type == nd1.type){
-                        fx += dx / dist / dist / dist * 30;
-                        fy += dy / dist / dist / dist * 30;
-                    };
-                    //なんとなく集合しようとする
-                    if(dist>1){
-                        var vx = nd1.vx;
-                        var vy = nd1.vy;
-                        var rad = Math.atan2(vy,vx);
-                        fx += Math.cos(rad) /dist/dist;
-                        fy += Math.sin(rad) /dist/dist;
-                    };
-                    //加速度を集積
-                    nd0.fx += fx;
-                    nd0.fy += fy;
-                    };
-                };
-                //加速度をノーマライズ
-                var fdist = Math.sqrt(nd0.fx * nd0.fx + nd0.fy * nd0.fy);
-                if(fdist > 1){
-                    nd0.fx = nd0.fx/fdist;
-                    nd0.fy = nd0.fy/fdist;
-                };
-                //若干ランダムに運動
-                nd0.fx += (Math.random()-0.5)*0.2;
-                nd0.fy += (Math.random()-0.5)*0.2;
-                };
-                //力積の適用
-                for(var i=0; i<numberOfNodes;i++){
-                    Geo.Circle.collection[i].vx += Geo.Circle.collection[i].fx;
-                    Geo.Circle.collection[i].vy += Geo.Circle.collection[i].fy;
-                    var vvx =  Geo.Circle.collection[i].vx;
-                    var vvy =  Geo.Circle.collection[i].vy;
-                    var vdist = Math.sqrt(vvx * vvx + vvy * vvy);
-                    //速度をノーマライズ
-                    if(vdist > 1){
-                        Geo.Circle.collection[i].vx = vvx/vdist;
-                        Geo.Circle.collection[i].vy = vvy/vdist;
-                    };
-
-                    //画面外に出る速度がある場合速度を反転
-                    var sumX = Geo.Circle.collection[i].x + Geo.Circle.collection[i].vx;
-                    var sumY = Geo.Circle.collection[i].y + Geo.Circle.collection[i].vy;
-                    if(sumX < 5 || sumX > enchant.Core.instance.width-5){
-                        Geo.Circle.collection[i].vx *= -1;
-                    };
-                    if(sumY < 5 || sumY > enchant.Core.instance.height-5){
-                        Geo.Circle.collection[i].vy *= -1;
-                    };
-
-                    //既に画面外にいる場合中心力をかける
-                    sumX -= Geo.Circle.collection[i].vx;
-                    sumY -= Geo.Circle.collection[i].vy;
-                    if(sumX < 0 || sumX > enchant.Core.instance.width){
-                        Geo.Circle.collection[i].vx = (enchant.Core.instance.width - sumX)/10;
-                    }
-                    if(sumY < 0 || sumY > enchant.Core.instance.height){
-                        Geo.Circle.collection[i].vy = (enchant.Core.instance.height - sumY)/10;
-                    };
-
-                    //クリック移動中は力積が適用されない（演算自体を飛ばしたほうが効率的）
-                    if(Geo.Circle.collection[i].isCapture == true && Cell.userTouch.capComed == true){
-                        //nothing
-                    }else{
-                        //速度の適用
-                        Geo.Circle.collection[i].x += Geo.Circle.collection[i].vx;
-                        Geo.Circle.collection[i].y += Geo.Circle.collection[i].vy;
-                        //整数値にならす(GPU環境では特に不要)
-                        Geo.Circle.collection[i].x = (Geo.Circle.collection[i].x + 0.5) << 0;
-                        Geo.Circle.collection[i].y = (Geo.Circle.collection[i].y + 0.5) << 0;
-                    }
-                };
-                //力積の適用項内だとクリック時の相対座標保持に問題がありそうなので、別個に処理
-                if(Cell.userTouch.capComed == true){
-                    for(var i=0;i<Cell.userTouch.capture.length;i++){
-                        Cell.userTouch.capture[i].x = Cell.userTouch.x + Cell.userTouch.capture[i].rVecX;
-                        Cell.userTouch.capture[i].y = Cell.userTouch.y + Cell.userTouch.capture[i].rVecY;
-                    };
-                };
-            };
-        });
+        Cell.addDrawLineEvent();
+        //セル間の相互作用による運動の計算
+        Cell.addForceEvent();
     };
     game.start();
 };
